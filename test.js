96878a2748deea3b7538b391f76bed292ec6ae8f
Ian Boll <mainbollian@gmail.com>
2024-02-24 12:48:19 -0800

    Fix fs paths and `tree-sitter test` execution on windows
    Addresses #12

--- test.js
+++ test.js
@@ -1,52 +1,41 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { spawn } = require('node:child_process');
 const { describe, test } = require('node:test');
 const Parser = require('tree-sitter');
 const OpenScad = require('.');
 const assert = require('node:assert').strict;
 
-const openscad_tests_root = 'examples/openscad/tests/data/scad';
+const openscad_tests_root = path.join('examples', 'openscad', 'tests', 'data', 'scad');
 const expected_failed_parses = [
-  '2D/features/text-search-test.scad',
-  '3D/features/polyhedron-nonplanar-tests.scad',
-  'customizer/allexpressionscomment.scad',
-  'functions/assert-expression-fail1-test.scad',
-  'functions/assert-expression-fail2-test.scad',
-  'functions/assert-expression-tests.scad',
-  'functions/echo-expression-tests.scad',
-  'functions/expression-precedence-tests.scad',
-  'functions/let-tests.scad',
-  'functions/trig-tests.scad',
-  'issues/issue1851-each-fail-on-scalar.scad',
-  'misc/allexpressions.scad',
-  'misc/assert-fail5-test.scad',
-  'misc/assert-tests.scad',
-  'misc/let-module-tests.scad',
-  'misc/nbsp-latin1-test.scad',
-  'misc/nbsp-utf8-test.scad',
-  'misc/no-break-space-test.scad',
-  'misc/parser-tests.scad',
-  'misc/scope-assignment-tests.scad',
-  'misc/tail-recursion-tests.scad',
-  'misc/variable-overwrite.scad',
+  path.join('2D', 'features', 'text-search-test.scad'),
+  path.join('3D', 'features', 'polyhedron-nonplanar-tests.scad'),
+  path.join('customizer', 'allexpressionscomment.scad'),
+  path.join('functions', 'assert-expression-fail1-test.scad'),
+  path.join('functions', 'assert-expression-fail2-test.scad'),
+  path.join('functions', 'assert-expression-tests.scad'),
+  path.join('functions', 'echo-expression-tests.scad'),
+  path.join('functions', 'expression-precedence-tests.scad'),
+  path.join('functions', 'let-tests.scad'),
+  path.join('functions', 'trig-tests.scad'),
+  path.join('issues', 'issue1851-each-fail-on-scalar.scad'),
+  path.join('misc', 'allexpressions.scad'),
+  path.join('misc', 'assert-fail5-test.scad'),
+  path.join('misc', 'assert-tests.scad'),
+  path.join('misc', 'let-module-tests.scad'),
+  path.join('misc', 'nbsp-latin1-test.scad'),
+  path.join('misc', 'nbsp-utf8-test.scad'),
+  path.join('misc', 'no-break-space-test.scad'),
+  path.join('misc', 'parser-tests.scad'),
+  path.join('misc', 'scope-assignment-tests.scad'),
+  path.join('misc', 'tail-recursion-tests.scad'),
+  path.join('misc', 'variable-overwrite.scad'),
 ];
 const ignored_examples = [
-  'issues/issue1890-comment.scad',
-  'issues/issue1890-include.scad',
-  'issues/issue1890-string.scad',
-  'issues/issue1890-use.scad',
+  path.join('issues', 'issue1890-comment.scad'),
+  path.join('issues', 'issue1890-include.scad'),
+  path.join('issues', 'issue1890-string.scad'),
+  path.join('issues', 'issue1890-use.scad'),
 ];
-
-test('tree-sitter test', async (_t) => {
-  const ts_test = spawn('npx', ['tree-sitter', 'test'], {
-    windowsHide: true,
-  });
-  await new Promise(resolve => ts_test.on('exit', code => {
-    assert.strictEqual(code, 0);
-    resolve();
-  }));
-});
 
 describe('Upstream OpenSCAD test samples', () => {
   const parser = new Parser();
